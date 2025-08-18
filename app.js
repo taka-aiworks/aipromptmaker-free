5b137f2fe14c672d3a1fdf02808d3356a3891289
taka-aiworks <guidajiben3@gmail.com>
2025-08-18 19:04:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
    LoRA Prompt Maker – app.js
    （分割版 / 軽量化込み）
    ========================= */
-
+console.log = function(...args){
+  alert(JSON.stringify(args));
+}
 
 /* ========= ユーティリティ & 状態 ========= */
 const $  = (s) => document.querySelector(s);
